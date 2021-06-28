9a53c71f391db942ce49e91d8a9c38135cccc424
lohenyumnam <lohenyumnam@users.noreply.github.com>
2021-06-28 11:23:52 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.61816e8db20b78452b57bfebf1e88555.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd6aa995f7c4193a35dde06c6a399253",
+    "url": "https://lohenyumnam.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "63fb615f5ebae910fc46",
+    "url": "https://lohenyumnam.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "f00b87f7f881c1ac50f1",
+    "url": "https://lohenyumnam.github.io/task-management-frontend/static/js/2.3f232f48.chunk.js"
+  },
+  {
+    "revision": "63fb615f5ebae910fc46",
+    "url": "https://lohenyumnam.github.io/task-management-frontend/static/js/main.c7aaae04.chunk.js"
+  },
+  {
+    "revision": "3378d7afecf97efdfa3e",
+    "url": "https://lohenyumnam.github.io/task-management-frontend/static/js/runtime~main.12574e0f.js"
+  }
+]);
